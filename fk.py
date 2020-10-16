5d3b12f6377311e9f2bb7b1cd94bb80fec58f74e
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-10-17 00:25:25 +0530

    Load dotenv file in environment

--- fk.py
+++ fk.py
@@ -6,6 +6,8 @@ import time
 import sys
 from __banner__.banner import banner
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from dotenv import load_dotenv, find_dotenv
+import os
 
 
 ######## This is only for educational purpose ########
@@ -38,6 +40,9 @@ def main():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     }
+
+    # Check for dotenv file and load into environment
+    load_dotenv(find_dotenv())
 
     chk_list_urls = []
     no = 1
